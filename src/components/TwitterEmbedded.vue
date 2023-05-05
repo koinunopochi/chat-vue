2cc7e59a4eab091906e40b16bcc4b8768dc5f5f3
Okayama <okapi771205a@gmail.com>
2023-05-06 01:37:54 +0900

    Twitterの実装

--- src/components/TwitterEmbedded.vue
+++ src/components/TwitterEmbedded.vue
@@ -1,18 +1,33 @@
 <template>
-  <form class="twitter">
-    本当はTwitterが入る
-  </form>
+  <div>
+    <a
+      ref="twitterTimeline"
+      class="twitter-timeline"
+      href="https://twitter.com/a1a2a3b1b2b3b4?ref_src=twsrc%5Etfw"
+      >Tweets by home</a
+    >
+  </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    const script = document.createElement('script');
+    script.src = 'https://platform.twitter.com/widgets.js';
+    script.charset = 'utf-8';
+    script.async = true;
+    this.$refs.twitterTimeline.parentNode.appendChild(script);
+  },
+};
+</script>
+
 <style scoped>
-form {
-  background-color: #ffffff;
-  width: 250px;
-  padding: 20px;
+div{
   margin-top: 50px;
-  margin-left: 30px;
-  margin-right: 20px;
-  border-radius: 10px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  margin-right:15px ;
+  width: 300px;
+  height: 800px;
+  overflow: auto;/**コンテナサイズを超えた場合スクロールバーを表示する */
 }
 </style>
 
